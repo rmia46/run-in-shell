aee76674acb54c95acef4ca8203164fd0eb93adf
Roman Mia <54774634+rmia46@users.noreply.github.com>
2024-04-08 15:15:28 +0600

    Add powershell-config file via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,126 @@
+# function to run source files and then write the output to output.txt file
+# It looks for input in the input.txt file
+# usage runp <filename>
+function runp {
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$filename
+    )
+
+    $extension = [System.IO.Path]::GetExtension($filename).ToLower()
+
+    $inputFile = "input.txt"
+    $outputFile = "output.txt"
+
+    switch ($extension) {
+        ".c" {
+            $output = & gcc $filename -o output.exe 2>&1
+            if ($LASTEXITCODE -eq 0) {
+                & Get-Content $inputFile | .\output.exe | Set-Content $outputFile
+            } else {
+                Write-Output $output
+            }
+        }
+
+        ".cpp" {
+            $output = & g++ $filename -Wall -o output.exe 2>&1
+            if ($LASTEXITCODE -eq 0) {
+                & Get-Content $inputFile | .\output.exe | Set-Content $outputFile
+            } else {
+                Write-Output $output
+            }
+        }
+
+        ".java" {
+            $classname = [System.IO.Path]::GetFileNameWithoutExtension($filename)
+            $output = & javac $filename 2>&1
+            if ($LASTEXITCODE -eq 0) {
+                & Get-Content $inputFile| java $classname | Set-Content $outputFile
+            } else {
+                Write-Output $output
+            }
+        }
+
+        ".py" {
+            & Get-Content $inputFile | python $filename | Set-Content $outputFile
+        }
+
+        ".rb" {
+            & Get-Content $inputFile | ruby $filename | Set-Content $outputFile
+        }
+
+        ".js" {
+            & Get-Content $inputFile | node $filename | Set-Content $outputFile
+        }
+
+        ".lua" {
+            & Get-Content $inputFile | lua $filename | Set-Content $outputFile
+        }
+
+        default {
+            Write-Output "Unsupported Source File"
+        }
+    }
+}	
+
+# function to run source files and then show the output to shell
+# usage run <filename>
+
+function run {
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$filename
+    )
+
+    $extension = [System.IO.Path]::GetExtension($filename).ToLower()
+
+    switch ($extension) {
+        ".c" {
+            $output = & gcc $filename -o output.exe 2>&1
+            if ($LASTEXITCODE -eq 0) {
+                & .\output.exe
+            } else {
+                Write-Output $output
+            }
+        }
+
+        ".cpp" {
+            $output = & g++ $filename -o output.exe 2>&1
+            if ($LASTEXITCODE -eq 0) {
+                & .\output.exe
+            } else {
+                Write-Output $output
+            }
+        }
+
+        ".java" {
+            $classname = [System.IO.Path]::GetFileNameWithoutExtension($filename)
+            $output = & javac $filename 2>&1
+            if ($LASTEXITCODE -eq 0) {
+                & java $classname
+            } else {
+                Write-Output $output
+            }
+        }
+
+        ".py" {
+            & python $filename
+        }
+
+        ".rb" {
+            & ruby $filename
+        }
+
+        ".js" {
+            & node $filename
+        }
+
+        ".lua" {
+            & lua $filename
+        }
+
+        default {
+            Write-Output "Unsupported file extension: $extension"
+        }
+    }
+}
